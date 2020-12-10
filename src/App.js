3cc9b2cdef9a345cd90223c23762a56c6f8d4a0a
dallasclarke <dallas.ag.clarke@gmail.com>
2020-12-10 14:08:24 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useState } from "react";
+
+import Search from "./Search/Search";
+import Movie from "./Movie/Movie";
+import MoveDetails from "./Movie/MovieDetails";
+import "./App.css";
+
+function AppNav() {
+  const [searchValue, setSearchValue] = useState("");
+  const [movieResults, setMovieResults] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  async function fetchMovieListAPI(inputValue) {
+    setSearchValue(inputValue);
+
+    const MOVIE_API_KEY = process.env.REACT_APP_MOVIE_OMDB_API;
+
+    try {
+      const response = await fetch(
+        `http://omdbapi.com/?apikey=${MOVIE_API_KEY}&s=${inputValue}`
+      );
+
+      const data = await response.json();
+
+      if (!data.Error) {
+        setIsFetching(true);
+        setMovieResults(data.Search);
+      }
+
+      setMovieResults(data.Search || []);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return (
+    <div className="App">
+      <Search
+        searchValue={searchValue}
+        fetchMovieListAPI={fetchMovieListAPI}
+        movieResults={movieResults}
+        isFetching={isFetching}
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={AppNav} />
+        <Route exact path="/:movieTitle" component={Movie} />
+        <Route render={() => <h1>Not Found</h1>} />
+      </Switch>
+    </Router>
   );
 }
 
